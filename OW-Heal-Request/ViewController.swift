fb3a1a8f20e0a3bc471d6a11097e47f5055322f3
Andrew Draper <andy.draper83@gmail.com>
2020-12-28 14:50:12 -0600

    Initial Commit

---
+++ OW-Heal-Request/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OW-Heal-Request
+//
+//  Created by The Clout on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
